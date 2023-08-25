c0ae0cc0a3d3fa701922b3e17117c8c078522569
rajasree2002 <131839512+rajasree2002@users.noreply.github.com>
2023-08-25 11:19:12 +0530

    Update settings.py

--- CollegeERP/settings.py
+++ CollegeERP/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jy8c-n9y=pf##!2^jae-l_5iafq6q%wfq8gdb6c0r5d52su+9y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 AUTH_USER_MODEL = 'info.User'
 
